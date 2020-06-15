5d5e0b54355f2af1ff00aa238e540bf8c0775986
Luke MacKenzie <mackenzieluke94@gmail.com>
2020-06-15 15:49:19 -0400

    more notes

--- react-testing-library-course/src/__tests__/tdd-01-markup.js
+++ react-testing-library-course/src/__tests__/tdd-01-markup.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {render} from '@testing-library/react'
 import {Editor} from '../post-editor-01-markup'
 
+// write all this out before coding the Editor form
+// this leaves you with the task of writing Editor so that all tests pass (i.e. Test Driven Development)
 test('renders a form with title, content, tags, and a submit button', () => {
   const {getByLabelText, getByText} = render(<Editor />)
   getByLabelText(/title/i)
